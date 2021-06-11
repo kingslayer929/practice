3f748ad8d798b9f06f2a87dc59763b1a16009bef
kingslayer929 <percylin0929@gmail.com>
2021-06-11 17:13:37 +0800

    add tolower.c & update copy.c

--- tolower.c
+++ tolower.c
@@ -2,33 +2,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define MAX_BUF 9192
+#include <ctype.h>
+#include <string.h>
+#define MAX_BUF 1024
 
 void usage(char *path){
     fprintf(stderr, "usage:\n");
-    fprintf(stderr, "%s filename destination\n", path);
+    fprintf(stderr, "%s filename\n", path);
 }
 
 int main(int argc, char **argv){
-    if(argc != 3){
+    if(argc != 2){
         usage(argv[0]);
         exit(1);
     }
-    int fd_in, fd_out;
+    int fd_in, fd_out, readsize;
     char buf[MAX_BUF];
     fd_in = open(argv[1], O_RDONLY);
+    fd_out = open(argv[1], O_WRONLY);
     if(!fd_in){
         fprintf(stderr, "open %s error\n", argv[1]);
         exit(1);
     }
-    fd_out = open(argv[2], O_CREAT | O_WRONLY, MAX_BUF);
-    if(fd_out){
-        while(read(fd_in, buf, 1)){
-            write(fd_out, buf, 1);
-        }
-    }else{
-        fprintf(stderr, "failed to open/create %s\n", argv[1]);
-        exit(1);
+    while((readsize = read(fd_in, buf, 1))){
+        buf[0] = tolower(buf[0]);
+        write(fd_out, buf, 1);
     }
 
     return 0;
